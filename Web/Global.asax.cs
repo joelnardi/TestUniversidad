f66a11150a29952387de4fa711444539f722e657
Joel Nardi <jnardi@frro.utn.edu.ar>
2019-04-16 18:51:25 -0300

    Actualización. Subida de paquetes necesarios.
    .

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,26 +19,3 @@ namespace Web
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace Web
-{
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-    }
-}
->>>>>>> parent of 34a34cb... Eliminado viejo folder de Web
